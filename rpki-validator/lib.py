ee0884c80c593279e068f041d2c78af107b19d23
Pawel Foremski <pjf@foremski.pl>
2024-05-17 12:59:49 +0200

    less comments for slides

--- rpki-validator/lib.py
+++ rpki-validator/lib.py
@@ -1,23 +1,6 @@
-import sys
-import json
-import requests
+import sys, json, requests
 
-# routinator HTTP validity check URL
-URL = "http://127.0.0.1:31339/api/v1/validity"
-
-# rpki_check returns false iff prefix+origin is RPKI invalid
-def rpki_check(origin: int, prefix: str) -> bool:
-	try:
-		url = f"{URL}/{origin}/{prefix}"
-		response = requests.get(url)
-		response.raise_for_status()
-		result = response.json()
-		return result["validated_route"]["validity"]["state"] != "invalid"
-	except Exception as err:
-		print(err, file=sys.stderr)
-		raise # pass the exception
-
-# parse_json parses a message and checks if it announces IPv4 prefixes
+# parse BGP message and check if it announces IPv4 prefixes
 def parse_json(line: str) -> object:
 	msg = json.loads(line)
 	if msg[4] != "UPDATE" or len(msg[5]["reach"]) == 0:
@@ -25,3 +8,17 @@ def parse_json(line: str) -> object:
 	if "unreach" not in msg[5]:
 		msg[5]["unreach"] = list() # in case it's needed later
 	return msg
+
+# return false iff prefix+origin is RPKI invalid
+def rpki_check(origin: int, prefix: str) -> bool:
+	URL = "http://127.0.0.1:31339/api/v1/validity"
+	try:
+		url = f"{URL}/{origin}/{prefix}"
+		response = requests.get(url)
+		response.raise_for_status()
+		result = response.json()
+		return result ["validated_route"] \
+			["validity"]["state"] != "invalid"
+	except Exception as err:
+		print(err, file=sys.stderr)
+		raise
--- rpki-validator/validator.py
+++ rpki-validator/validator.py
@@ -6,23 +6,21 @@ import sys, lib, json
 
 for line in sys.stdin:
 	try:
-		# parse the message
 		msg = lib.parse_json(line)
 
-		# validate each prefix against the origin AS
+		# validate prefixes vs. the origin AS
 		origin = int(msg[5]["attrs"]["ASPATH"]["value"][-1])
 		ok, fail = [], []
 		for pfx in msg[5]["reach"]:
 			ok.append(pfx) if lib.rpki_check(origin, pfx) \
 				else fail.append(pfx)
 
-		# all prefixes ok?
-		if len(fail) == 0:
+		# treat invalid prefixes as withdrawn, like rfc7606
+		if len(fail) > 0:
+			msg[5]["reach"] = ok
+			msg[5]["unreach"].extend(fail)
+			print(json.dumps(msg), flush=True) # rewrite msg
+		else:
 			raise # accept as-is
-
-		# treat failed prefixes as withdrawn, like rfc7606
-		msg[5]["reach"] = ok
-		msg[5]["unreach"].extend(fail)
-		print(json.dumps(msg), flush=True) # modify the original message
 	except:
-		print(line, end="", flush=True)    # accept the message as-is
+		print(line, end="", flush=True)
